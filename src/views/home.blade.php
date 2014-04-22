56346a6362fc534bb6f1319a2751501e0755696e
Peter Caulfield <peterjcaulfield@gmail.com>
2014-04-22 17:47:31 +0100

    base for new filter setup on front end

--- src/views/home.blade.php
+++ src/views/home.blade.php
@@ -2,24 +2,49 @@
 
 @section('content')
 <?php
-
     $queryString = Request::query();
     unset($queryString['page']);
-    $typeSelect = isset($queryString['type']) ? $queryString['type'] : '';
-    $dateSelect = isset($queryString['date']) ? $queryString['date'] : '';
+    $filters = isset($queryString['filters']) ? $queryString['filters'] : '';
     $searchQuery = isset($queryString['search']) ? $queryString['search'] : '';
 ?>
 <script>
     $(document).ready(function(){
 
-        var typeSelect = "<?php echo $typeSelect; ?>";
-        var dateSelect = "<?php echo $dateSelect; ?>";
+        var filterTypes = {
+            'today' : 'date',
+            'week' : 'date',
+            'month' : 'date',
+            'year' : 'date',
+            'image' : 'format',
+            'video' : 'format',
+            'audio' : 'format',
+        }
+
+        var filterString = "<?php echo $filters; ?>";
         var searchQuery = "<?php echo $searchQuery; ?>";
 
-        if ( typeSelect )
-            $("#filter option[value='" + typeSelect + "']").attr('selected', 'selected');
-        if ( dateSelect )
-            $("#filterDate option[value='" + dateSelect + "']").attr('selected', 'selected');
+        var filters = filterString.split(',');
+
+        $('.filter-options a').each(function(){
+            var href = $(this).attr('href');
+
+            var newHref = '';
+
+            if ( filters.length )
+            {
+                for ( var i = 0; i < filters.length; i++ )
+                {
+                    // if href doesn't contain filter already and if filter and href are not both date filters append the filter
+                    if ( href.indexOf(filters[i]) == -1 && !( filterTypes[href.substr(20)] == 'date' && filterTypes[filters[i]] == 'date' ) )
+                        newHref += ',' + filters[i];
+                }
+            }
+
+            if ( searchQuery !== '')
+                newHref += '&search=' + searchQuery;
+
+            $(this).attr('href', href + newHref);
+        });
 
         $('#searchFilter').val(searchQuery);
 
@@ -39,6 +64,11 @@
                checkbox.removeClass('unchecked').addClass('checked');
             else
                checkbox.removeClass('checked').addClass('unchecked');
+        });
+
+        $('#filter-action').on('click', function(){
+            console.log('hurr');
+            $('#filters').toggle();
         });
 
         $('#applyBatch').on('click', function(){
@@ -75,43 +105,39 @@
                 <option value="0">Actions</option>
                 <option value="1" id="uploadBatchApply">Move to trash</option>
             </select>
-            <button id="applyBatch" class="btn btn-sm btn-primary">Apply</button>
+            <button id="applyBatch" class="btn btn-default btn-sm">Apply</button>
         </div>
-        <div id="filters">
+        <button type="button" id="filter-action" class="btn btn-default btn-sm">Filters <i class="fa fa-caret-down"></i></button>
+        <div id="search">
             <form method="get" action="<?php echo URL::route('uploadyodaHome'); ?>" id="filterForm">
-                <select name="type" id="filter" class="uploadInput form-control">
-                    <option value="0">all types</option>
-                    <option value="image">images</option>
-                    <option value="video">video</option>
-                </select>
-                <select name="date" id="filterDate" class="uploadInput form-control">
-                    <option value="0">all dates</option>
-                    <option value="1" >january</option>
-                    <option value="2">feburary</option>
-                    <option value="3">march</option>
-                    <option value="4">april</option>
-                    <option value="5" >may</option>
-                    <option value="6">june</option>
-                    <option value="7" >july</option>
-                    <option value="8">august</option>
-                    <option value="9">september</option>
-                    <option value="10">october</option>
-                    <option value="11">november</option>
-                    <option value="12">december</option>
-                </select>
                 <div class="input-group" id="searchContainer">
                     <input id="searchFilter" type="text" class="form-control" name="search">
                     <span class="input-group-btn">
-                        <button class="btn btn-sm btn-primary" type="submit">Search</button>
+                        <button class="btn btn-default btn-sm" type="submit">Search</button>
                     </span>
                 </div>
-                <button id="applyFilter" class="btn btn-sm btn-primary">Filter</button>
             </form>
+        </div>
+        <div id="filters">
+            <div class="filter-options">
+                <p><strong>Upload date</strong></p>
+                <p><a href="/uploadyoda?filters=today">Today</a></p>
+                <p><a href="/uploadyoda?filters=week">This week</a></p>
+                <p><a href="/uploadyoda?filters=month">This month</a></p>
+                <p><a href="/uploadyoda?filters=year">This year</a></p>
+            </div>
+            <div class="filter-options">
+                <p><strong>Result type</strong></p>
+                <p><a href="/uploadyoda?filters=image">Image</a></p>
+                <p><a href="/uploadyoda?filters=video">Video</a></p>
+                <p><a href="/uploadyoda?filters=audio">Audio</a></p>
+            </div>
+            <div class="filter-options">
+                <p><strong>Sort by</strong></p>
+            </div>
         </div>
     </div>
     <table id="header-container" class="table table-condensed table-bordered" style="position: relative">
-    <img id="loading-mask-gif" class="loading-mask-elem" src="/packages/quasimodal/uploadyoda/img/loader.gif">
-        <div id="loading-mask" class="loading-mask-elem"></div>
         <th id="thumbnail-header" class="header"><div class="header-text"><input type="checkbox" id="uploadCheckboxBatch" class=""></div></th>
         <th id="name-header" class="header"><div class="header-text">Name</div></th>
         <th id="size-header" class="header"><div class="header-text">Size</div></th>
